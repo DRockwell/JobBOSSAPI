0edc970e9e3f85443174523ea5fafe5d345864f3
David Rockwell <davidwrockwell@gmail.com>
2017-07-27 21:02:24 -0400

    Added demo project and project references. Updated target framework to 4.6.2

---
+++ JobbossAPI/JobbossAPI/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobbossAPI
+{
+    /// <summary>
+    /// Will eventually set this up to read an external config file
+    /// </summary>
+    public static class Settings
+    {
+        public static string ConnectionString = @"JOBBOSS_CONNECTION_STRING";
+    }
+}
